dd545394d2d1f7386da4bf0c403a237567625b27
Hua Zhang <huazhan@microsoft.com>
2018-08-29 10:00:46 -0700

    Refactor storage type names which are case insensitive

--- asa-manager/Services/Runtime/ServicesConfig.cs
+++ asa-manager/Services/Runtime/ServicesConfig.cs
@@ -2,13 +2,13 @@
 
 namespace Microsoft.Azure.IoTSolutions.AsaManager.Services.Runtime
 {
-    // Types of storage where ASA writes messages and alarms
+    // Types of storage where ASA writes messages and alarms and case insensitive
     public enum AsaOutputStorageType
     {
         CosmosDbSql = 1,
-        cosmosdb = CosmosDbSql,
+        CosmosDb = CosmosDbSql,
         TimeSeriesInsights = 2,
-        tsi = TimeSeriesInsights
+        Tsi = TimeSeriesInsights
     }
 
     public interface IServicesConfig
